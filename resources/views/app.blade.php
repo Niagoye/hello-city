c9eb1edf712e430cbe42e4f623e225f3a986400d
Alassane Niane <amaniane97@gmail.com>
2020-11-29 22:14:40 +0000

    don't add about as link in about page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,11 @@
   @yield('contente')
 
     <footer>
-           <p>&copy; Copyright {{ date('Y') }} &middot; <a href="{{route('about')}}">A propos</a></p>
+           <p>&copy; Copyright {{ date('Y') }} &middot; 
+           @if(!Route::is('about'))
+           <a href="{{route('about')}}">A propos</a> 
+           @endif
+           </p>
     </footer> 
     </body>
 </html>
